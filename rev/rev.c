a176c5d6e9b15086e2907a0e662a0fc0778da1d3
tom960541 <122672339+tom960541@users.noreply.github.com>
2023-10-31 08:04:28 +0800

    Update rev.c

--- rev/rev.c
+++ rev/rev.c
@@ -1,188 +1,70 @@
 #include <stdio.h>
-#define EMPTY 0
-#define BLACK 1
-#define WHITE 2
-int chess[8][8];
-void initializeBoard()
+//參考yinxiehaung
+
+int main()
 {
+    int chess[8][8] = {}, address_x = 0, address_y = 0;
+    int direction[8][2] = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
+    // 陣列direction表示八個可能的移動方向
+
     for (int i = 0; i < 8; i++)
     {
         for (int j = 0; j < 8; j++)
         {
-            chess[i][j] = EMPTY;
+            scanf("%1d", &chess[i][j]);
+            // 1d可以按單位讀取
         }
     }
-    // 设置初始棋盘状态
-    chess[3][3] = WHITE;
-    chess[3][4] = BLACK;
-    chess[4][3] = BLACK;
-    chess[4][4] = WHITE;
-}
 
-int determinexy(int i, int j, int color)
-{
-    if (chess[i][j] == BLACK)
-        printf("black");
-    if (chess[i][j] == WHITE)
-        printf("white");
-    if (chess[i][j] != WHITE && chess[i][j != BLACK])
-        printf("empty");
-}
-int direction[8][2] = {
-    {1, 1},
-    {1, 0},
-    {1, -1},
-    {0, 1},
-    {0, -1},
-    {-1, 0},
-    {-1, 1},
-    {-1, -1}};
+    scanf("%d,%d", &address_x, &address_y);
 
-int the8directionOf(int x, int y, int dx, int dy)
-{
-    x = dx + x;
-    y = dy + y;
-}
+    if (chess[address_y][address_x] == 0)
+    {
+        printf("(%d,%d) is 0", address_x, address_y);
+        return 0;
+    }
 
-int validMove(int x, int y, int color)
-{
-    int opponent = (color == BLACK) ? WHITE : BLACK;
-    int j = 0, q = 0, w = 0, e = 0, r = 0, t = 0, j1 = 0, j2 = 0, j3 = 0, j4 = 0, j5 = 0, j6 = 0;
-    for (int i = 0; i < 8; i++)
-    {
-        the8directionOf(x, y, direction[i][0], direction[i][1]);
-        while (chess[x][y] == EMPTY)
-        {
-            while (chess[x][y - j] == opponent)
-            { // 測左邊
-                j++;
-                while (chess[x][y - j] == EMPTY)
-                {
-                    printf("%d%d is valid moeve", x, y - j);
-                }
-            }
-        }
-    }
-    for (int i = 0; i < 8; i++)
-    {
-        the8directionOf(x, y, direction[i][0], direction[i][1]);
-        while (chess[x][y] == EMPTY)
-        {
-            while (chess[x - q][y - w] == opponent)
-            { // 左上
-                q++;
-                w++;
-                while (chess[x - q][y - w] == EMPTY)
-                {
-                    printf("%d%d is valid move", x - q, y - w);
-                }
-            }
-        }
-    }
-    for (int i = 0; i < 8; i++)
-    {
-        the8directionOf(x, y, direction[i][0], direction[i][1]);
-        while (chess[x][y] == EMPTY)
-        {
-            while (chess[x + e][y] == opponent)
-            { // 下
-                e++;
-                while (chess[x + e][y] == EMPTY)
-                {
-                    printf("%d%d is valid move", x + e, y);
-                }
-            }
-        }
-    }
-    for (int i = 0; i < 8; i++)
-    {
-        the8directionOf(x, y, direction[i][0], direction[i][1]);
-        while (chess[x][y] == EMPTY)
-        {
-            while (chess[x + r][y - t] == opponent)
-            { // 左下
-                r++;
-                t++;
-                while (chess[x + r][y - t] == EMPTY)
-                {
-                    printf("%d%d is valid", x + r, y - t);
-                }
-            }
-        }
-    }
-    for (int i = 0; i < 8; i++)
-    {
-        the8directionOf(x, y, direction[i][0], direction[i][1]);
-        while (chess[x][y] == EMPTY)
-        {
-            while (chess[x + j1][y + j2] == opponent)
-            { // 右下
-                j1++;
-                j2++;
-                while (chess[x + j1][y + j2] == EMPTY)
-                {
-                    printf("%d%d is valid", x + j1, x + j2);
-                }
-            }
-        }
-    }
-    for (int i = 0; i < 8; i++)
-    {
-        the8directionOf(x, y, direction[i][0], direction[i][1]);
-        while (chess[x][y] == EMPTY)
-        {
-            while (chess[x][y + j3] == opponent)
-            { // 右
-                j3++;
-                while (chess[x][y + j3] == EMPTY)
-                {
-                    printf("%d%d is valid", x, y + j3);
-                }
-            }
-        }
-    }
-    for (int i = 0; i < 8; i++)
-    {
-        the8directionOf(x, y, direction[i][0], direction[i][1]);
-        while (chess[x][y] == EMPTY)
-        {
-            while (chess[x - j4][y] == opponent)
-            { // up
-                j4++;
-                while (chess[x + j4][y] == EMPTY)
-                {
+    int color = chess[address_y][address_x];
 
-                    printf("%d%d is valid", x + j4, y);
-                }
-            }
-        }
-    }
     for (int i = 0; i < 8; i++)
     {
-        the8directionOf(x, y, direction[i][0], direction[i][1]);
-        while (chess[x][y] == EMPTY)
+        int x = address_x;
+        int y = address_y;
+        int con = 0;
+        // 八個可能的移動方向
+        for (int k = 0; k < 8; k++)
         {
-            while (chess[x - j5][y + j6] == opponent)
+            x += direction[i][0];
+            y += direction[i][1];
+            // 按照當前方向移動x,y座標
+
+            if ((x >= 0 && x < 8) && (y >= 0 && y < 8))
             {
-                j5++;
-                j6++;
-                while (chess[x - j5][y + j6] == EMPTY)
+                if (chess[y][x] == 0 && con)
                 {
-                    printf("%d%d is valid", x - j5, x + j6);
+                    chess[y][x] = 120;
+                    break;
+                    // 120(因為120以字元輸出的話是x)
                 }
+                con = (chess[y][x] == 3 - color) ? 1 : 0;
+                // 如果(x,y)為對手的顏色則con=1不是則為0
             }
         }
     }
-}
 
-int main()
-{
-    int x, y;
-    int color = chess[x][y];
-    initializeBoard();
-    scanf("%d%d", &x, &y);
-    determinexy(x, y, color);
-    validMove(x, y, color);
-
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            if (chess[i][j] == 120)
+            {
+                printf("%c ", chess[i][j]);
+                continue;
+            }
+            printf("%d ", chess[i][j]);
+            // 输出每個位置的值
+        }
+        printf("\n");
+    }
     return 0;
 }
